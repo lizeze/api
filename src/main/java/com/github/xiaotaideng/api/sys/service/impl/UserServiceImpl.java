0a7f3e141eca163b5eeb2d50b450757c48a83243
zeze.li <lizeze.boy@gmail.com>
2020-11-19 16:39:12 +0800

    增加自定义注解

--- src/main/java/com/github/xiaotaideng/api/sys/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/xiaotaideng/api/sys/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.github.xiaotaideng.api.sys.service.impl;
 
+import com.github.xiaotaideng.api.core.annotation.AnalysisActuator;
 import com.github.xiaotaideng.api.core.base.BaseService;
 import com.github.xiaotaideng.api.core.base.BaseServiceImpl;
 import com.github.xiaotaideng.api.mapper.UserMapper;
 import com.github.xiaotaideng.api.sys.model.UserDO;
 import com.github.xiaotaideng.api.sys.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author zeze.li
@@ -14,4 +17,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends BaseServiceImpl<UserMapper, UserDO> implements UserService {
+    @AnalysisActuator(note = "获取用户")
+    @Override
+    public List<UserDO> userList(UserDO userDO) {
+        return this.list();
+    }
 }
